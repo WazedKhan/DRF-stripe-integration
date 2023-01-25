68a317b546d15bacfa29ed2d45b8061dee09884b
WazedKhan <wazedkhan119399@gmail.com>
2023-01-26 00:56:51 +0600

    product

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "products",
+    "pay",
+    "stripe",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=250)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
